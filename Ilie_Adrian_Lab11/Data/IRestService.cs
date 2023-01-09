6f23c952b71ee447006bd8f2bab0e16a3a10c4b7
Adrian Ilie <ilie.adrian1@yahoo.com>
2023-01-09 10:54:58 +0200

    Add project files.

---
+++ Ilie_Adrian_Lab11/Data/IRestService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ilie_Adrian_Lab11.Models;
+
+namespace Ilie_Adrian_Lab11.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
